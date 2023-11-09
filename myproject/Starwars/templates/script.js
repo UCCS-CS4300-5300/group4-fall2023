c23170736d6f7f5b27cd5a5deb44914914eab5fa
EricaJesse <ejesse@uccs.edu>
2023-11-09 07:35:07 +0000

    added selection,landing and script files

---
+++ myproject/Starwars/templates/script.js
@@ -0,0 +1,13 @@
+
+const handleLoginPage = () => {
+	window.location.href = "login.html"
+}
+
+const handleSignUp = () => {
+	window.location.href = "signup.html"
+}
+
+const handleSelectionPage = () => {
+	window.location.href = "selection.html"
+}
+
