d1cca820cc83abc071147dd4ce762c842e4da29c
Replit user <>
2023-11-21 16:40:29 +0000

    all packages

--- replit.nix
+++ replit.nix
@@ -1,6 +1,10 @@
 { pkgs }: {
   deps = [
-    pkgs.python39Packages.pip
+    pkgs.python38Packages.coverage
+    pkgs.httpie
     pkgs.python39Packages.django
+    pkgs.python39Packages.pip
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server  
   ];
 }
