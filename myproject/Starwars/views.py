f87b3f6465a4606575187394460efad73a5456b2
Replit user <>
2023-11-21 18:06:13 +0000

    before superuser

--- myproject/Starwars/views.py
+++ myproject/Starwars/views.py
@@ -7,13 +7,18 @@ from .forms import StarWarsForm
 import urllib.request
 import json
 from django.core.exceptions import ObjectDoesNotExist
+import os
 
 
-TIMELINE_TO_FILM_IDS = {
-    '1': [1, 2, 3],  # Episode 1-3
-    '4': [4, 5, 6],  # Episode 4-6
-    '7': [7, 8, 9]   # Episode 7-9
-}
+def load_json_data(file_name):
+  base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  json_file_path = os.path.join(base_dir, file_name)
+  with open(json_file_path, 'r', encoding='utf-8') as file:
+      return json.load(file)
+
+# Load the JSON data at the start of the application
+starwars_data = load_json_data('starwars_data.json')
+
 
 # Create your views here.
 class HomeView(View):
@@ -27,29 +32,35 @@ class LoginView(View):
   def get(self, request):
     return render(request, "login.html")
 
+
 class StartView(View):
-	def get(self, request):
-		return render(request, "start.html")
+
+  def get(self, request):
+    return render(request, "start.html")
+
 
 class Player1View(View):
 
-	def get(self, request):
-		return render(request, "player1.html")
+  def get(self, request):
+    return render(request, "player1.html")
+
 
 class Player2View(View):
 
-	def get(self, request):
-		return render(request, "player2.html")
+  def get(self, request):
+    return render(request, "player2.html")
+
 
 class BattleView(View):
 
-	def get(self, request):
-		return render(request, "battle.html")
+  def get(self, request):
+    return render(request, "battle.html")
+
 
 class SelectPlayer2View(View):
 
-	def get(self, request):
-		return render(request, "selectPlayer2.html")
+  def get(self, request):
+    return render(request, "selectPlayer2.html")
 
 
 class SignupView(View):
@@ -57,10 +68,11 @@ class SignupView(View):
   def get(self, request):
     return render(request, "signup.html")
 
+
 class SelectionView(View):
 
-	def get(self, request):
-		return render(request, "selection.html")
+  def get(self, request):
+    return render(request, "selection.html")
 
 
 class ProfileView(View):
@@ -68,93 +80,90 @@ class ProfileView(View):
   def get(self, request):
     return render(request, "profile.html")
 
+
 class GameplayView(TemplateView):
   template_name = "gameplay.html"
 
   def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context['form'] = StarWarsForm()  # Add the form to the context
-      return context
+    context = super().get_context_data(**kwargs)
+    context['form'] = StarWarsForm()  # Add the form to the context
+    return context
+
 
 class EndscreenView(View):
+
   def get(self, request, battle_id):
-      battle = Battle.objects.get(id=battle_id)
-      characters = Character.objects.filter(
-          episode_from__lte=battle.episode_to,
-          episode_to__gte=battle.episode_from
-      )
-      context = {
-          'win_status': 'You Win' if battle.won else 'You Lose',
-          'battle_details': battle.details,
-          'characters': characters
-      }
-      return render(request, 'endscreen.html', context)
+    battle = Battle.objects.get(id=battle_id)
+    characters = Character.objects.filter(episode_from__lte=battle.episode_to,
+                                          episode_to__gte=battle.episode_from)
+    context = {
+        'win_status': 'You Win' if battle.won else 'You Lose',
+        'battle_details': battle.details,
+        'characters': characters
+    }
+    return render(request, 'endscreen.html', context)
+
 
 def get_character_name(character_url):
   with urllib.request.urlopen(character_url) as response:
-      character_data = json.loads(response.read().decode())
-      return character_data['name']
+    character_data = json.loads(response.read().decode())
+    return character_data['name']
+
 
 def load_characters(request):
   episode_id = request.GET.get('episode_id')
-  url = f'https://swapi.dev/api/films/{episode_id}/'
+
+  if not episode_id:  # Check if episode_id is not empty
+    return JsonResponse(
+        [], safe=False)  # Return an empty list or handle as appropriate
 
   try:
-      with urllib.request.urlopen(url) as response:
-          data = json.loads(response.read().decode())
-          characters_urls = data.get('characters', [])
-          characters = []
-          for character_url in characters_urls:
-              character_name = get_character_name(character_url)
-              characters.append({'name': character_name, 'url': character_url})
-          return JsonResponse(characters, safe=False)
-  except urllib.error.HTTPError as e:
-      # Log error
-      print(f'HTTPError: {e.code} for film ID: {episode_id}')
-      return JsonResponse({'error': 'The requested resource was not found.'}, status=404)
+    characters = Character.objects.filter(episodes__id=episode_id)
+    characters_data = [{
+        'name': character.name,
+        'id': character.id
+    } for character in characters]
+    return JsonResponse(characters_data, safe=False)
   except Exception as e:
-      # Log any other exceptions
-      print(f'An error occurred: {e}')
-      return JsonResponse({'error': 'An unexpected error occurred.'}, status=500)
+    print(f'An error occurred: {e}')
+    return JsonResponse({'error': 'An unexpected error occurred.'}, status=500)
+
 
 def load_starships(request):
-    character_id = request.GET.get('character_id')
-    url = f'https://swapi.dev/api/people/{character_id}/'
+  character_id = request.GET.get('character_id')
 
-    try:
-        with urllib.request.urlopen(url) as response:
-            character_data = json.loads(response.read().decode())
-            starship_urls = character_data.get('starships', [])
-            starships = []
-            for starship_url in starship_urls:
-                with urllib.request.urlopen(starship_url) as response:
-                    starship_data = json.loads(response.read().decode())
-                    starships.append({
-                        'name': starship_data['name'],
-                        'model': starship_data['model']
-                    })
-            return JsonResponse(starships, safe=False)
-    except urllib.error.HTTPError as e:
-        print(f'HTTPError: {e.code} when fetching starships for character ID: {character_id}')
-        return JsonResponse({'error': 'Starship resource not found.'}, status=e.code)
-    except Exception as e:
-        print(f'An error occurred: {e}')
-        return JsonResponse({'error': 'An unexpected error occurred.'}, status=500)
+  try:
+    character = Character.objects.get(id=character_id)
+    starships = character.starships.all()
+    starships_data = [{
+        'name': starship.name,
+        'id': starship.id
+    } for starship in starships]
+    return JsonResponse(starships_data, safe=False)
+  except Character.DoesNotExist:
+    return JsonResponse({'error': 'Character not found.'}, status=404)
+  except Exception as e:
+    print(f'An error occurred: {e}')
+    return JsonResponse({'error': 'An unexpected error occurred.'}, status=500)
 
 
 def get_characters_from_episode(episode_id):
-    url = f'https://swapi.dev/api/films/{episode_id}/'
-    with urllib.request.urlopen(url) as response:
-        data = json.loads(response.read().decode())
-        characters = data.get('characters', [])
-        return characters
+  try:
+    episode = Episode.objects.get(id=episode_id)
+    characters = episode.characters.all()
+    return [character.name for character in characters]
+  except Episode.DoesNotExist:
+    return []
+
 
 def get_starships_for_character(character_id):
-    url = f'https://swapi.dev/api/people/{character_id}/'
-    with urllib.request.urlopen(url) as response:
-        data = json.loads(response.read().decode())
-        starships = data.get('starships', [])
-        return starships
+  try:
+    character = Character.objects.get(id=character_id)
+    starships = character.starships.all()
+    return [starship.name for starship in starships]
+  except Character.DoesNotExist:
+    return []
+
 
 def your_view(request):
   form = StarWarsForm()
