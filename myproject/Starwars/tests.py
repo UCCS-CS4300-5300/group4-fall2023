213e18c37d170cc59126afe6dfc3ffbf88d14235
EricaJesse <ejesse@uccs.edu>
2023-12-05 23:39:09 -0700

    fixed syntax

--- myproject/Starwars/tests.py
+++ myproject/Starwars/tests.py
@@ -14,7 +14,7 @@ class CharacterModelTest(TestCase):
 class ViewsTest(TestCase):
 
     def test_home_view(self):
-        rgit push origin --tags esponse = self.client.get(reverse('home'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
